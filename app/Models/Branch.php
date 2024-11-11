936fa12fb18de3d1824d1bb25cf0bf7b4e9a9fed
ScottForster400 <scottforster400@gmail.com>
2024-11-11 13:14:56 +0000

    Added users relation

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -3,11 +3,17 @@
 namespace App\Models;
 
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Branch extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function users(){
+        return $this->hasMany(User::class);
+    }
 }
