06fdb08d03beb1a93c0da6a903975acac80ce00a
ScottForster400 <scottforster400@gmail.com>
2024-11-11 14:57:17 +0000

    Added Eloquent relationships

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,6 +41,7 @@
             </div>
         </main>
 
+        <x-application-logo></x-application-logo>
 
         <footer class="flex justify-center items-end basis-1/4 pb-10 text-center text-sm text-black dark:text-white/70  ">
             Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
