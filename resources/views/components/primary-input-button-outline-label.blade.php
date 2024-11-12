235f7148944096cab867e2642106e4fdb64e6567
ScottForster400 <scottforster400@gmail.com>
2024-11-12 18:06:56 +0000

    sidebar-complete

---
+++ resources/views/components/primary-input-button-outline-label.blade.php
@@ -0,0 +1,2 @@
+<label {{$attributes->merge(['class' => ' text-center select-none cursor-pointer rounded-lg border-2 border-indigo-500
+    py-3 px-6 font-bold text-gray-800 transition-colors duration-200 ease-in-out peer-checked:bg-indigo-500 peer-checked:text-gray-900 peer-checked:border-indigo-500'])}}>{{$slot}}</label>
