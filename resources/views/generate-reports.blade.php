84789a0deb06fd8f9f6117e0479b707843a9d481
matkuz161 <1panmateuszkuzon@gmail.com>
2024-11-12 12:32:22 +0000

    reports for each transaction

--- resources/views/generate-reports.blade.php
+++ resources/views/generate-reports.blade.php
@@ -11,12 +11,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
 
-                <x-modal-toggle>Generate Report</x-modal-toggle>
-                <x-modal>
-                    <x-modal-header>Report</x-modal-header>
-                    <x-modal-body>This is my report</x-modal-body>
-                    <x-modal-footer>Close</x-modal-footer>
-                </x-modal>
+
 
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
@@ -35,7 +30,14 @@
                             @foreach($transaction as $key => $data)
                                 <x-tr>
                                     <x-th>{{$data->transaction_id}}</x-th>
-                                    <x-th>{{$data->price}}</x-th>
+                                    <x-th>
+                                        <x-modal-toggle>Generate Report</x-modal-toggle>
+                                        <x-modal>
+                                            <x-modal-header>Report</x-modal-header>
+                                            <x-modal-body>{{$data->price}}</x-modal-body>
+                                            <x-modal-footer>Close</x-modal-footer>
+                                        </x-modal>
+                                    </x-th>
                                 </x-tr>
                             @endforeach
                         </x-table-body>
