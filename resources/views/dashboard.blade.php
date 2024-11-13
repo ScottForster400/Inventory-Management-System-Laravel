231a899c9e100c43a958383bd0958f9a377a0940
ScottForster400 <scottforster400@gmail.com>
2024-11-13 17:31:10 +0000

    Added stock index chexk

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
-    <x-slot name="header">
+    <x-slot name="header" >
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
     <div class="flex flex-row w-full">
         @include('layouts.sidebar')
-        <div class="py-12 basis-3/4">
+        <div class=" w-full py-12  flex items-center flex-col ">
             <div class="flex flex-3/4 pl-20 pr-20 max-w-8/10 w-full">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-full">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
