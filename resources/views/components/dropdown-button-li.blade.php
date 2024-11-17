96d522b6bfb5a6bf3014aa2a8d88ee4e909fc1eb
ScottForster400 <scottforster400@gmail.com>
2024-11-17 18:34:39 +0000

    mobile serach and filter display -partial

---
+++ resources/views/components/dropdown-button-li.blade.php
@@ -0,0 +1,5 @@
+<li>
+    <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+        {{$slot}}
+    </div>
+  </li>
--- resources/views/components/sidebar-list.blade.php
+++ resources/views/components/sidebar-list.blade.php
@@ -1,3 +1,3 @@
-<li class="flex flex-row justify-evenly flex-wrap w-full">
+<li {{ $attributes->merge([ 'class' => 'flex flex-row justify-evenly flex-wrap w-full'])}}>
     {{$slot}}
 </li>
