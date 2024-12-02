7a59b4241b09ac07e9b25158cac6c7d07c513c21
ScottForster400 <scottforster400@gmail.com>
2024-12-02 13:26:36 +0000

    edit stock partially complete

--- resources/views/layouts/edit-stock-modal.blade.php
+++ resources/views/layouts/edit-stock-modal.blade.php
@@ -3,42 +3,62 @@
     <x-modal-header data-modal-hide="edit{{$product->product_id}}">
         Edit Stock
     </x-modal-header>
-    <form action="">
+    <form action="{{route('dashboard.update',$product)}}" method="post">
+        @method('put')
+        @csrf
         <x-modal-body>
                 <x-table>
                     <x-table-body>
                         <x-tr class="max-sm:flex max-sm:flex-col">
                             <x-td class="max-sm:w-full">
-                                <x-input-label for="min-price" :value="__('Product Name')" >Product Name</x-input-label>
-                                <x-text-input id="min-price" type="text" name="name" class="w-full basis-1/2" value="{{$product->name}}"></x-text-input>
+                                <x-input-label for="name" :value="__('Product Name')" >Product Name</x-input-label>
+                                <x-text-input id="name" type="text" name="name" class="w-full basis-1/2" value="{{$product->name}}"></x-text-input>
+                                @error('name')
+                                <x-error-message-form>{{$message}}</x-error-message-form>
+                                @enderror
                             </x-td>
                             <x-td >
                                 <x-input-label for="price" :value="__('Price(£)')">Price(£)</x-input-label>
                                 <x-text-input id="price" type="number" name="price"  class="w-full basis-1/2" value="{{$product->Price}}"></x-text-input>
+                                @error('price')
+                                    <x-error-message-form>{{$message}}</x-error-message-form>
+                                @enderror
                             </x-td>
                             <x-td>
                                 <x-input-label for="manufacturer" :value="__('Manufacturer')" >Manufacturer</x-input-label>
-                                <x-text-input id="manufacturer" type="text" name="Manufacturer" class="w-full basis-1/2" value="{{$product->manufacturer}}"></x-text-input>
+                                <x-text-input id="manufacturer" type="text" name="manufacturer" class="w-full basis-1/2" value="{{$product->manufacturer}}"></x-text-input>
+                                @error('manufacturer')
+                                    <x-error-message-form>{{$message}}</x-error-message-form>
+                                @enderror
                             </x-td>
                         </x-tr>
                         <x-tr class="max-sm:flex max-sm:flex-col">
                             <x-td>
-                                <x-input-label for="age-rating" :value="__('Age Rating')" >Age Rating</x-input-label>
-                                <x-text-input id="age-rating" type="number" name="age-rating" min=0  class="w-full basis-1/2" value="{{$product->age_rating}}"></x-text-input>
+                                <x-input-label for="age_rating" :value="__('Age Rating')" >Age Rating</x-input-label>
+                                <x-text-input id="age_rating" type="number" name="age_rating" min=0  class="w-full basis-1/2" value="{{$product->age_rating}}"></x-text-input>
+                                @error('age')
+                                <x-error-message-form>{{$message}}</x-error-message-form>
+                                 @enderror
                             </x-td>
                             <x-td>
                                 <x-input-label for="amount" :value="__('Amount')">Amount</x-input-label>
-                                <x-text-input id="amount" type="number" name="player-count" min=0 class="w-full basis-1/2" value="{{$stock[$int]->amount}}"></x-text-input>
+                                <x-text-input id="amount" type="number" name="amount" min=0 class="w-full basis-1/2" value="{{$stock[$int]->amount}}"></x-text-input>
+                                @error('amount')
+                                    <x-error-message-form>{{$message}}</x-error-message-form>
+                                @enderror
                             </x-td>
                             <x-td>
-                                <x-input-label for="game-length" :value="__('Game Length')" >Game Length</x-input-label>
-                                <x-text-input id="game-length" type="number" name="game-length" min=0 class="w-full basis-1/2" value="{{$product->game_length}}"></x-text-input>
+                                <x-input-label for="game_length" :value="__('Game Length')" >Game Length</x-input-label>
+                                <x-text-input id="game-length" type="number" name="game_length" min=0 class="w-full basis-1/2" value="{{$product->game_length}}"></x-text-input>
+                                @error('game-length')
+                                    <x-error-message-form>{{$message}}</x-error-message-form>
+                                @enderror
                             </x-td>
                         </x-tr>
                         <x-tr class="max-sm:flex max-sm:flex-col">
                             <x-td>
-                                <x-input-label for="game-type" :value="__('Game Type')" >Game Type</x-input-label>
-                                <x-select id="game-type"  class="w-full basis-1/2">
+                                <x-input-label for="game_type" :value="__('Game Type')" >Game Type</x-input-label>
+                                <x-select id="game_type" name="game_type" class="w-full basis-1/2">
                                     @if ($product->game_type == "board_game")
                                         <option value="board_game" selected>Board Game</option>
                                         <option value="puzzle_game">Puzzles</option>
@@ -73,8 +93,8 @@
                                 </x-select>
                             </x-td>
                             <x-td class="">
-                                <x-input-label for="game-genre" :value="__('Game Genre')" >Game Genre</x-input-label>
-                                <x-select id="game-genre"  class="w-full basis-1/2">
+                                <x-input-label for="game_genre" :value="__('Game Genre')" >Game Genre</x-input-label>
+                                <x-select id="game_genre"  name="game_genre" class="w-full basis-1/2">
                                 @if ($product->game_genre == "adventure")
                                     <option value="adventure" selected>Adventure</option>
                                     <option value="puzzle">Puzzle</option>
@@ -109,14 +129,20 @@
                                 </x-select>
                             </x-td>
                             <x-td>
-                                <x-input-label for="player-count" :value="__('Min Players')">Min Players</x-input-label>
-                                <x-text-input id="player-count" type="number" name="player-count" min=0 class="w-full basis-1/2" value="{{$product->minimum_player_count}}"></x-text-input>
+                                <x-input-label for="min_players" :value="__('Min Players')">Min Players</x-input-label>
+                                <x-text-input id="min_players" type="number" name="min_players" min=0 class="w-full basis-1/2" value="{{$product->minimum_player_count}}"></x-text-input>
+                                @error('min-players')
+                                    <x-error-message-form>{{$message}}</x-error-message-form>
+                                @enderror
                             </x-td>
                         </x-tr>
                         <x-tr class="max-sm:flex max-sm:flex-col">
                             <x-td>
-                                <x-input-label for="player-count" :value="__('Max Players')">Max Players</x-input-label>
-                                <x-text-input id="player-count" type="number" name="player-count" min=0 class="w-full basis-1/2" value="{{$product->maximum_player_count}}"></x-text-input>
+                                <x-input-label for="max_players" :value="__('Max Players')">Max Players</x-input-label>
+                                <x-text-input id="max_players" type="number" name="max-players" min=0 class="w-full basis-1/2" value="{{$product->maximum_player_count}}"></x-text-input>
+                                @error('max-players')
+                                    <x-error-message-form>{{$message}}</x-error-message-form>
+                                @enderror
                             </x-td>
                         </x-tr>
                     </x-table-body>
@@ -124,10 +150,13 @@
                 <div class="w-full">
                     <x-input-label for="description" :value="__('Description')" >Description</x-input-label>
                     <textarea id="description" rows="4" class="block resize-none p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Description..." >{{$product->description}}</textarea>
+                    @error('description')
+                        <x-error-message-form>{{$message}}</x-error-message-form>
+                    @enderror
                 </div>
             </x-modal-body>
-        <x-modal-footer>
-            Edit Stock
-        </x-modal-footer>
+            <div class="flex items-center max-sm:justify-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <x-primary-button class="!py-3 !bg-blue-700 hover:!bg-blue-800 !transition-colors">Edit Stock</x-primary-button>
+            </div>
     </form>
 </x-modal>
