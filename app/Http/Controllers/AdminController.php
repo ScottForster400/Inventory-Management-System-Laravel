04b1deb0cd1abadad33749db5728a344bfc739b3
matkuz161 <1panmateuszkuzon@gmail.com>
2024-12-06 11:12:10 +0000

    current

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,9 +11,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index(Request $request)
     {
 
