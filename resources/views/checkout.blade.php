e927f8687b017d7e7249a6f0dd435b354f6ca39a
Zephyri0s <149779192+Zephyri0s@users.noreply.github.com>
2024-12-16 11:40:43 +0000

    Im a big sigma

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -5,7 +5,7 @@
 
 
 
-<div class="flex max-md:flex-col max-md:items-center justify-between p-4 w-full">
+<div class="flex max-md:flex-col max-md:items-center items-center justify-between p-4 w-full">
     <div class="w-1/4 border-solid border-2 border-gray-500 rounded-lg shadow-xl p-4 max-md:w-4/5">
         @include('layouts/checkout-box')
     </div>
@@ -13,9 +13,9 @@
         $int = 0;
     @endphp
     @if ($carts)
-        <div class="flex flex-row flex-wrap justify-center items-center">
+        <div class="flex flex-row flex-wrap justify-center items-center w-full">
             @foreach ($carts as $cart)
-                <div class="flex md:flex-col max-md:min-w-64 max-md:w-2/5 max-md:flex-row max-md:flex-wrap items-center w-1/4 bg-white border border-gray-500 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-4">
+                <div class="flex md:flex-col max-md:min-w-64 max-md:w-2/5 max-md:flex-row max-md:flex-wrap justify-center items-center w-1/4 bg-white border border-gray-500 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-4">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="{{$products[$int]->image}}" alt="{{$products[$int]->name}}">
                     <div class="flex flex-col justify-between p-4 leading-normal text-center items-center">
                         <span class="flex mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$products[$int]->name}}</span>
